00d3c074582581949d431cc3efa79d75e843c740
Rafael Konlechner <r.konlechner@gmail.com>
2015-04-14 18:34:14 +0200

    Init project

---
+++ SBCWallstreet/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight;
+using SBCWallstreet.Model;
+
+namespace SBCWallstreet.ViewModel
+{
+
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+        public MainViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+                });
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
